8d38c214ff65daf3ba3aad5bf2633e597be57b47
martink-io <martinkiogit@gmail.com>
2017-11-25 22:45:18 +0000

    Add Artifact

---
+++ app/models/artifact.rb
@@ -0,0 +1,18 @@
+class Artifact < ApplicationRecord
+  attr_accessor :upload
+
+  belongs_to :project
+
+  validates_presence_of :name, :upload
+  validates_uniqueness_of :name
+  validate :uploaded_file_size  
+
+  MAX_FILESIZE = 10.megabytes
+
+  private
+    def uploaded_file_size
+      if upload
+        errors.add(:upload, "File size must be less than #{self.class::MAX_FILESIZE}") unless upload.size <= self.class::MAX_FILESIZE
+      end
+    end
+end
